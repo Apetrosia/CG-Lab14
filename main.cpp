4475580d0bb21f1f2f481797e9b67bf20c478b1d
Анна Петросян <apetrosia@sfedu.ru>
2024-12-21 16:30:47 +0300

    Add light type switching

--- main.cpp
+++ main.cpp
@@ -26,6 +26,9 @@ float lastFrame = 0.0f;
 
 // lighting
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+int lightType = 2;
+int newLightType = 2;
+
 void checkOpenGLerror() {
     /*
     GLenum error;
@@ -53,7 +56,6 @@ void checkOpenGLerror() {
     */
 }
 
-
 //void Release() {
 //    glDeleteVertexArrays(1, &VAO);
 //    glDeleteBuffers(1, &VBO);
@@ -75,22 +77,34 @@ void Init()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(sf::Window& window)
 {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+        newLightType = 0;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+        newLightType = 1;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+        newLightType = 2;
+
     // Проверка закрытия окна при нажатии ESC
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         window.close();
 
     // Управление камерой с помощью клавиш WASD
+    float v = deltaTime;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+        v *= 2.5;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-        camera.ProcessKeyboard(FORWARD, deltaTime);
+        camera.ProcessKeyboard(FORWARD, v);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
+        camera.ProcessKeyboard(BACKWARD, v);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        camera.ProcessKeyboard(LEFT, deltaTime);
+        camera.ProcessKeyboard(LEFT, v);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        camera.ProcessKeyboard(RIGHT, v);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)
+        || sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
         camera.ProcessKeyboard(UP, deltaTime);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)
+        || sf::Keyboard::isKeyPressed(sf::Keyboard::LControl))
         camera.ProcessKeyboard(DOWN, deltaTime);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         camera.ProcessKeyboard(ROTATE_LEFT, deltaTime);
@@ -156,6 +170,7 @@ unsigned int loadTexture(char const* path)
 
     return textureID;
 }
+
 int main()
 {
     setlocale(LC_ALL, "ru");
@@ -171,12 +186,15 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
+    // Проэктор
     //Shader lightingShader("5.4.light_casters.vs", "5.4.light_casters.fs");
     //Shader lightCubeShader("5.4.light_cube.vs", "5.4.light_cube.fs");
 
+    // Направленный источник света
     //Shader lightingShader("5.1.light_casters.vs", "5.1.light_casters.fs");
     //Shader lightCubeShader("5.1.light_cube.vs", "5.1.light_cube.fs");
 
+    // Точечный источник света
     Shader lightingShader("5.2.light_casters.vs", "5.2.light_casters.fs");
     Shader lightCubeShader("5.2.light_cube.vs", "5.2.light_cube.fs");
 
@@ -240,8 +258,6 @@ int main()
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
-    
-
     // first, configure the cube's VAO (and VBO)
     unsigned int VBO, cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
@@ -279,6 +295,7 @@ int main()
     lightingShader.setInt("material.diffuse", 0);
     lightingShader.setInt("material.specular", 1);
     sf::Clock clock;
+
     // Основной цикл
     while (window.isOpen())
     {
@@ -298,55 +315,87 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // be sure to activate shader when setting uniforms/drawing objects
-        /*lightingShader.use();
-        lightingShader.setVec3("light.position", camera.Position);
-        lightingShader.setVec3("light.direction", camera.Front);
-        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
-        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
-        lightingShader.setVec3("viewPos", camera.Position);
 
-        // light properties
-        lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
-        // we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
-        // each environment and lighting type requires some tweaking to get the best out of your environment.
-        lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        lightingShader.setFloat("light.constant", 1.0f);
-        lightingShader.setFloat("light.linear", 0.09f);
-        lightingShader.setFloat("light.quadratic", 0.032f);
+        // Проэктор
+        if (newLightType == 0)
+        {
+            if (newLightType != lightType)
+            {
+                lightingShader = Shader("5.4.light_casters.vs", "5.4.light_casters.fs");
+                lightCubeShader = Shader("5.4.light_cube.vs", "5.4.light_cube.fs");
+                lightType = newLightType;
+            }
 
-        // material properties
-        lightingShader.setFloat("material.shininess", 32.0f);*/
-        // be sure to activate shader when setting uniforms/drawing objects
+            lightingShader.use();
+            lightingShader.setVec3("light.position", camera.Position);
+            lightingShader.setVec3("light.direction", camera.Front);
+            lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+            lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+            lightingShader.setVec3("viewPos", camera.Position);
 
+            // light properties
+            lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+            // we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
+            // each environment and lighting type requires some tweaking to get the best out of your environment.
+            lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
+            lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+            lightingShader.setFloat("light.constant", 1.0f);
+            lightingShader.setFloat("light.linear", 0.09f);
+            lightingShader.setFloat("light.quadratic", 0.032f);
 
-        /*lightingShader.use();
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
-        lightingShader.setVec3("viewPos", camera.Position);
+            // material properties
+            lightingShader.setFloat("material.shininess", 32.0f);
+            // be sure to activate shader when setting uniforms/drawing objects
+        }
 
-        // light properties
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        // Направленный источник света
+        else if (newLightType == 1)
+        {
+            if (newLightType != lightType)
+            {
+                lightingShader = Shader("5.1.light_casters.vs", "5.1.light_casters.fs");
+                lightCubeShader = Shader("5.1.light_cube.vs", "5.1.light_cube.fs");
+                lightType = newLightType;
+            }
 
-        // material properties
-        lightingShader.setFloat("material.shininess", 32.0f);*/
+            lightingShader.use();
+            lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+            lightingShader.setVec3("viewPos", camera.Position);
 
-        // be sure to activate shader when setting uniforms/drawing objects
-        lightingShader.use();
-        lightingShader.setVec3("light.position", lightPos);
-        lightingShader.setVec3("viewPos", camera.Position);
+            // light properties
+            lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+            lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+            lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
-        // light properties
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        lightingShader.setFloat("light.constant", 1.0f);
-        lightingShader.setFloat("light.linear", 0.09f);
-        lightingShader.setFloat("light.quadratic", 0.032f);
+            // material properties
+            lightingShader.setFloat("material.shininess", 32.0f);
+        }
+        // Точечный источник света
+        else if (newLightType == 2)
+        {
+            if (newLightType != lightType)
+            {
+                lightingShader = Shader("5.2.light_casters.vs", "5.2.light_casters.fs");
+                lightCubeShader = Shader("5.2.light_cube.vs", "5.2.light_cube.fs");
+                lightType = newLightType;
+            }
 
-        // material properties
-        lightingShader.setFloat("material.shininess", 32.0f);
+            // be sure to activate shader when setting uniforms/drawing objects
+            lightingShader.use();
+            lightingShader.setVec3("light.position", lightPos);
+            lightingShader.setVec3("viewPos", camera.Position);
+
+            // light properties
+            lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+            lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+            lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+            lightingShader.setFloat("light.constant", 1.0f);
+            lightingShader.setFloat("light.linear", 0.09f);
+            lightingShader.setFloat("light.quadratic", 0.032f);
+
+            // material properties
+            lightingShader.setFloat("material.shininess", 32.0f);
+        }
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -379,26 +428,20 @@ int main()
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        // again, a lamp object is weird when we only have a spot light, don't render the light object
-        lightCubeShader.use();
-        lightCubeShader.setMat4("projection", projection);
-        lightCubeShader.setMat4("view", view);
-        model = glm::mat4(1.0f);
-        //model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        lightCubeShader.setMat4("model", model);
-
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // also draw the lamp object
-        lightCubeShader.use();
-        lightCubeShader.setMat4("projection", projection);
-        lightCubeShader.setMat4("view", view);
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        lightCubeShader.setMat4("model", model);
+        if (lightType == 2)
+        {
+            // also draw the lamp object
+            lightCubeShader.use();
+            lightCubeShader.setMat4("projection", projection);
+            lightCubeShader.setMat4("view", view);
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, lightPos);
+            model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+            lightCubeShader.setMat4("model", model);
+        }
 
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -412,38 +455,6 @@ int main()
                 window.close();
         }
     }
-    /*while (window.isOpen())
-    {
-        sf::Event event;
-        
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
 
-            if (event.type == sf::Event::MouseMoved)
-            {
-
-            }
-
-            if (event.type == sf::Event::MouseWheelScrolled)
-            {
-
-            }
-
-            if (event.type == sf::Event::KeyPressed)
-            {
-                switch (event.key.code)
-                {
-                
-                }
-            }
-        }
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        Draw();
-        window.display();
-    }*/
-
-    // Release();
     return 0;
 }
